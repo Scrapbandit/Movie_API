f1826a7bc2d932e3ee1d9ad7531960a641340be0
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-04-11 17:46:15 +0300

    adding the uuid

--- index.js
+++ index.js
@@ -1,51 +1,139 @@
-const express = require('express');
-morgan = require('morgan');
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      uuid = require('uuid'),
+      morgan = require('morgan');
+
+
 const app = express();
 
+app.use(bodyParser.json());
+
+
+
+let users = [
+  {
+    id: 1,
+    username: "0val",
+    email: "val.moul@gmail.com",
+    password: "vt@rt2022!",
+    birthday: "17/06/1990",
+    favorites: [],
+  },
+  {
+    id: 2,
+    username: "stan1",
+    email: "stan.mont@gmail.com",
+    password: "Stgf022g34",
+    birthday: "13/12/1984",
+    favorites: [],
+  }
+];
 
 let topMovies = [
   {
     title: 'Harry Potter and the Sorcerer\'s Stone',
-    year: '2001'
+    year: '2001',
+    genre: {
+      name: "fantasy",
+      description: "",
+    },
+    director: {
+      name: "Christopher Nolan",
+      birth: "1970",
+      death: "-",
+      bio: "",
+    },
+    actors: {},
+    imgURL: "",
   },
   {
     title: 'Lord of the Rings',
-    year: '2003'
+    year: '2003',
+    genre: {
+      name: "Fantasy",
+      description: "",
+    },
+    director: {
+      name: "Peter Jackson",
+      birth: "1962",
+      death: "-",
+      bio: "",
+    },
+    actors: {},
+    imgURL: "",
   },
   {
     title: 'Inception',
-    year: '2010'
+    year: '2010',
+    genre: {
+      name: "Science-Fiction",
+      description: "",
+    },
+    director: {
+      name: "Danny Devito",
+      birth: "1940",
+      death: "-",
+      bio: "",
+    },
+    actors: {},
+    imgURL: "",
   },
   {
     title: 'Gatsby',
-    year: '2013'
-  },
-  {
-    title: 'Dango',
-    year: '2012'
-  },{
-    title: 'Fight club',
-    year: '1999'
-  },
-  {
-    title: 'Mad Max',
-    year: '2015'
-  },
-  {
-    title: 'Mask',
-    year: '1994'
-  },
-  {
-    title: 'Seven',
-    year: '1995'
-  },
-  {
-    title: 'Interstellar',
-    year: '2014'
+    year: '2013',
+    genre: {
+      name: "Drama",
+      description: "",
+    },
+    director: {
+      name: "Mike Ronson",
+      birth: "1962",
+      death: "-",
+      bio: "",
+    },
+    actors: {},
+    imgURL: "",
   }
 ];
 
 app.use(morgan('common')); //add morgan middlewar library
+
+// Gets the list of data about ALL users
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+// Gets the data about a single user, by name
+
+app.get('/users/:name', (req, res) => {
+  res.json(users.find((user) =>
+    { return user.name === req.params.name }));
+});
+
+// Adds data for a new user to our list of users.
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newuser.id = uuid.v4();
+    users.push(newuser);
+    res.status(201).send(newuser);
+  }
+});
+
+// Deletes a user from our list by ID
+app.delete('/users/:id', (req, res) => {
+  let user = users.find((user) => { return user.id === req.params.id });
+
+  if (user) {
+    users = users.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('user ' + req.params.id + ' was deleted.');
+  }
+});
+
 
 // GET requests
 app.get('/', (req, res) => {
