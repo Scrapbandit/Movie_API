e229de0b0b9e53207e3d02be11867b65322bcbeb
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-04-21 09:56:09 +0300

    bodyparser urlencoded

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const express = require('express'),
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
  
 app.use(morgan('common')); //add morgan middlewar library
 
