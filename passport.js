bc4c812750440246bd7e091960fedf3b2014616e
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-04-28 18:54:52 +0200

    !user validate

--- passport.js
+++ passport.js
@@ -24,6 +24,11 @@ const passport = require('passport'),
         console.log('incorrect username');
         return callback(null, false, {message: 'Incorrect username or password.'});
       }
+
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
+      }
   
       console.log('finished');
       return callback(null, user);
