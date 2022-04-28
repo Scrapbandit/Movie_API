93d225f1cb6824a57b7beaaa00fdd406676bd37a
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-04-28 18:30:33 +0200

    userschema

--- models.js
+++ models.js
@@ -24,6 +24,14 @@ let movieSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
+
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
   
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
