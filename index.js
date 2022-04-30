1e45f406d828ca4db173aa55b1c153a6f0e71f2e
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-04-30 17:11:11 +0200

    Uri

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Movies = Models.Movie,
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://myFlixDBuser:<password>@cluster0.2epnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
       
 
