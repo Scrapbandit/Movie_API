8ea13c8caeb08e7fa2983da81bed97437ffd713b
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-05-10 17:43:28 +0200

    auth pass movies

--- index.js
+++ index.js
@@ -247,10 +247,7 @@ app.get('/documentation', (req, res) => {
 
 //GEt all movies
 app.get('/movies', 
-passport.authenticate('jwt', {
-  session: false
-}),
- (req, res) => {
+(req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
